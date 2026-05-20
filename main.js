d2956594696448d6a9777aa03ea9087a013df02c
Miya <223133229+mstepanovic1@users.noreply.github.com>
2026-05-20 15:53:21 -0500

    run functions and display them in html
    trying to make it so the functions in js file will run at the right time
    and will print the player and computer choice on html
    its not currently working
    player choice will print correctly on html
    computer choice on html shows undefined

--- main.js
+++ main.js
@@ -20,29 +20,26 @@ function getComputerChoice() {
     if (compChoice=="3") {
         compChoice = "sissors";
     }
-
+    cChoice.innerText = "Computer Choice: " + compChoice.innerText; //somehow i gotta call this/make it so when player selects a choice, it runs this function to get computer choice at same time, rn, its just displaying undefined
     return compChoice;
     
 }
 
-//console.log("Computer Choice: " + getComputerChoice());
-
 
 
 function getHumanChoice() { //function promts for player choice and returns it
-    let humanChoice = document.querySelector('.btn');            //prompt("Please Enter Choice").toLowerCase();
-    //humanChoice.addEventListener("click", () => humanChoice);
+    const btns = document.querySelectorAll("button");
+    const result = document.getElementById("result");
+    btns.forEach((button) => {
+        button.addEventListener("click", () => {
+            pChoice.innerText = "Your Choice: " + button.innerText;
+            let humanChoice = button.innerText;
+            return humanChoice;
+        })
+    })
 } 
 
-const btns = document.querySelectorAll("button");
-const result = document.getElementById("result");
-btns.forEach((button) => {
-    button.addEventListener("click", () => {
-        result.innerText = "Your Choice: " + button.innerText;
-    })
-})
 
-//console.log ("Human Choice: " + getHumanChoice());
 
 function playRound(humanChoice, compChoice) {
     if ((humanChoice=="rock" && compChoice=="sissors") || //all instances where human wins
@@ -71,19 +68,19 @@ const compSelection = getComputerChoice();
 console.log("Computer Score: " + compScore);
 console.log("Player Score: " + humanScore); */
 
-//function playGame() {
-   // for (let i = 0; i < 5; i++) {
-       // const humanSelection = getHumanChoice();
-        //const compSelection = getComputerChoice();  
-        /*console.log(playRound(humanSelection, compSelection));
+function playGame() {
+    for (let i = 0; i < 5; i++) {
+        const humanSelection = getHumanChoice();
+        const compSelection = getComputerChoice();  
+        console.log(playRound(humanSelection, compSelection));
         console.log("Player Choice: " + humanSelection + " vs " + "Computer Choice: " + compSelection);
         console.log("Computer Score: " + compScore);
-        console.log("Player Score: " + humanScore);*/
-   // }
+        console.log("Player Score: " + humanScore);
+    }
 
-//}
+}
 
-//console.log(playGame());
+playGame();
 
 console.log("Final Scores:");
 console.log("Computer Score: " + compScore);
