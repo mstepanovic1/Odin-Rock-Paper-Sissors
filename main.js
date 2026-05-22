7d802a6b4407d05c3f2b57ee96a71eb04c613ca8
Miya <223133229+mstepanovic1@users.noreply.github.com>
2026-05-22 16:05:20 -0500

    edit js file to return choices in html
    added divs to html to add room for choices, winner, and scores. updated
    js file to make it so when a button is clicked, it returns the name of
    the button for the player choice and it auto generates and prints a
    random computer choice on the website itself too.
    still need to figure out how why it takes 2 button clicks/selections
    before the player choice shows up
    also need to make the winner div show up. i thought i coded the js
    play round function to update winner div, but its not working and i'm
    not sure why yet

--- main.js
+++ main.js
@@ -10,17 +10,17 @@ function getComputerChoice() {
     toString(compChoice); //turns randomly generated number 1-3 into string
 
     if (compChoice=="1") {
-        compChoice = "rock";
+        compChoice = "Rock";
     }
     
     if (compChoice=="2") {
-        compChoice = "paper";
+        compChoice = "Paper";
     }
 
     if (compChoice=="3") {
-        compChoice = "sissors";
+        compChoice = "Scissors";
     }
-    cChoice.innerText = "Computer Choice: " + compChoice.innerText; //somehow i gotta call this/make it so when player selects a choice, it runs this function to get computer choice at same time, rn, its just displaying undefined
+    cChoice.innerText = "Computer Choice: " + compChoice; //somehow i gotta call this/make it so when player selects a choice, it runs this function to get computer choice at same time, rn, its just displaying undefined
     return compChoice;
     
 }
@@ -40,24 +40,32 @@ function getHumanChoice() { //function promts for player choice and returns it
 } 
 
 
+document.addEventListener('click', function(event) { //when any button is clicked, run play round function
+    if (event.target.tagName === "BUTTON") {
+        playGame();
+    }
+});
 
 function playRound(humanChoice, compChoice) {
-    if ((humanChoice=="rock" && compChoice=="sissors") || //all instances where human wins
+    if ((humanChoice=="rock" && compChoice=="scissors") || //all instances where human wins
     (humanChoice=="paper" && compChoice=="rock") ||
-    (humanChoice=="sissors" && compChoice=="paper")) {
+    (humanChoice=="scissors" && compChoice=="paper")) {
         humanScore++;
-        return "Player wins!";
+        Winner.innerText = "Player Wins!";
+        //return "Player wins!";
     }
 
     if ((humanChoice=="rock" && compChoice=="paper") || //all instances where human loses
-    (humanChoice=="paper" && compChoice=="sissors") ||
-    (humanChoice=="sissors" && compChoice=="rock")) {
+    (humanChoice=="paper" && compChoice=="scissors") ||
+    (humanChoice=="scissors" && compChoice=="rock")) {
         compScore++;
-        return "Computer wins!";
+        Winner.innerText = "Computer Wins!";
+        //return "Computer wins!";
     } 
 
     if (humanChoice==compChoice) { //in case of a tie
-        return "It's a tie";
+        Winner.innerText = "It's a tie!";
+        //return "It's a tie";
     }
 }
 
@@ -69,22 +77,23 @@ console.log("Computer Score: " + compScore);
 console.log("Player Score: " + humanScore); */
 
 function playGame() {
-    for (let i = 0; i < 5; i++) {
+    //for (let i = 0; i < 5; i++) {
         const humanSelection = getHumanChoice();
-        const compSelection = getComputerChoice();  
-        console.log(playRound(humanSelection, compSelection));
-        console.log("Player Choice: " + humanSelection + " vs " + "Computer Choice: " + compSelection);
-        console.log("Computer Score: " + compScore);
-        console.log("Player Score: " + humanScore);
-    }
+        const compSelection = getComputerChoice();
+        playRound(humanSelection, compSelection);  
+        //console.log(playRound(humanSelection, compSelection));
+        //console.log("Player Choice: " + humanSelection + " vs " + "Computer Choice: " + compSelection);
+        //console.log("Computer Score: " + compScore);
+        //console.log("Player Score: " + humanScore);
+    //}
 
 }
 
-playGame();
+//playGame();
 
-console.log("Final Scores:");
-console.log("Computer Score: " + compScore);
-console.log("Player Score: " + humanScore);
+//console.log("Final Scores:");
+//console.log("Computer Score: " + compScore);
+//console.log("Player Score: " + humanScore);
 
 if (compScore > humanScore) {
     console.log("Computer Wins");
